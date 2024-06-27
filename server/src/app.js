83d79daf4c7da4ee91644e53fd2657819cd11608
KonkoffAV <konkoffav@gmail.com>
2024-06-27 18:27:13 +0300

    kov8

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
-const cors = require("cors");
-const apiRouter = require("./routers/api.router");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
+require('dotenv').config();
+const cors = require('cors');
+const apiRouter = require('./routers/api.router');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const { PORT } = process.env;
 
 const corsConfig = {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-    credentials: true,
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
 };
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsConfig));
 
-app.use("/api/v1", apiRouter);
+app.use('/api/v1', apiRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server started at ${PORT} port`);
+  console.log(`Server started at ${PORT} port`);
 });
